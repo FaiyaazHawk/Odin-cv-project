6558fc55da7218243c1665bba28b04dd333200fc
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2022-08-26 21:03:37 -0400

    base components set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import React from "react"
+import Header from "./components/Header"
+import Body from "./components/Body"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div>
-
-    </div>
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
+    </div>    
   );
 }
 
