c3754689ac58c8fc94236c0acf3d3dfdc300e771
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2022-08-27 22:47:07 -0400

    got form to stop sumitting default

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import General from './General'
 import Education from './Education'
 import Work from './Work'
+
 export default class Body extends Component {
   render() {
     return (
--- src/components/General.js
+++ src/components/General.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 
 export default class General extends Component {
+  constructor() {
+    super();
+    this.renderSection = this.renderSection.bind(this)
+  }
+
+  renderSection() {
+    console.log('test')
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+  
+
+
   render() {
     return (
       <div>
-        <form className='General'>
+        <form onSubmit={this.handleSubmit} className='General'>
             <label htmlFor='name'>Name:</label>
             <input type="text"></input>
             <label htmlFor='email'>Email:</label>
             <input type="email"></input>
             <label htmlFor='phone'>Phone:</label>
             <input type="number"></input>
-            <button type='submit'>ADD</button>
+            <button onClick={this.renderSection}>ADD</button>
         </form>
       </div>
     )
